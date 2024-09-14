8cd6501f24d167006b07957d7f06e5ef296cbb0e
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2024-09-14 02:30:42 -0500

    template de la pantalla de inicio

---
+++ src/constans.js
@@ -0,0 +1,17 @@
+export const apartamentosHechos = [
+    {
+      id: 1,
+      nombre: 'Casa lujosa',
+      arrendador: 'Daniel Lopez',
+    },
+    {
+      id: 2,
+      nombre: 'Casa humilde',
+      arrendador: 'Juan Perez',
+    },
+    {
+      id: 3,
+      nombre: 'Casa de campo',
+      arrendador: 'Maria Rodriguez',
+    },
+  ]
