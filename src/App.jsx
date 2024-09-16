e5edfab554c879f829d772463a64aaf31a30228c
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2024-09-16 11:32:46 -0500

    conexion  de prueba con backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
-import { apartamentosHechos } from './constans.js'
+import { cargarPropiedades, cargarArrendadores } from './constans.js'
 import { Encabezado } from './components/Encabezado.jsx'
 import { Apartamento } from './components/Apartamento.jsx'
 import './App.css'
 
+let propiedades = await cargarPropiedades();
+console.log(propiedades);
+let arrendadores = await cargarArrendadores();
+let name = arrendadores[0].first_name + ' ' + 'Perez';
 
 function App() {
-  const [apartamentos, setApartamentos] = useState(apartamentosHechos)
+
+
+  const [apartamentos, setApartamentos] = useState(propiedades)
   const createApartamento = () => {
     const newApartamento = {
       id: apartamentos.length + 1,
-      nombre: 'Casa de playa',
-      arrendador: 'Carlos Hernandez',
+      arrendador: 2,
+      descripcion: "Casa de playa con piscina y vista al mar",
+      direccion: "Calle 15 carrera 10b #20-30",
+      estado: "disponible",
+      fotos: "linkfotos.com",
+      precio: 120000,
+      reglas: "No hay",
+      servicios: "Ninguno",
+      tipo_vivienda: "Casa de playa",
+      videos: "linkvideos.com",
     }
 
     const apartamentosNuevos = [...apartamentos]
@@ -22,15 +36,16 @@ function App() {
   return (
     <main>
 
-       <Encabezado createApartamento={createApartamento}></Encabezado>
+      <Encabezado createApartamento={createApartamento}></Encabezado>
 
       {/* Mapeo de los apartamentos */}
       <section className='grid'>
         {apartamentos.map((apartamento) => (
           <Apartamento
             key={apartamento.id}
-            nombreApartamento={apartamento.nombre}
-            nombreArrendador={apartamento.arrendador} ></Apartamento>
+            nombreApartamento={apartamento.tipo_vivienda}
+            nombreArrendador={name}
+            estado={apartamento.estado}></Apartamento>
         ))}
       </section>
 
--- src/constans.js
+++ src/constans.js
@@ -1,17 +1,39 @@
-export const apartamentosHechos = [
-    {
-      id: 1,
-      nombre: 'Casa lujosa',
-      arrendador: 'Daniel Lopez',
-    },
-    {
-      id: 2,
-      nombre: 'Casa humilde',
-      arrendador: 'Juan Perez',
-    },
-    {
-      id: 3,
-      nombre: 'Casa de campo',
-      arrendador: 'Maria Rodriguez',
-    },
-  ]
+const urlPropiedades = 'https://backend-cocoon-project.onrender.com/api/propiedades/';
+const urlArrendadores = 'https://backend-cocoon-project.onrender.com/api/arrendadores/';
+export const cargarArrendadores = async () => {
+  const array = [];
+  // const res = await fetch('data.json');
+  const res = await fetch(urlArrendadores, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Token b2f57cf638b25ddd02390317de5017852342ab97'
+    }
+  });
+  const data = await res.json();
+
+  data.forEach(propiedad => {
+    array.push(propiedad);
+
+  });
+
+  return array;
+};
+
+export const cargarPropiedades = async () => {
+  const array = [];
+  // const res = await fetch('data.json');
+  const res = await fetch(urlPropiedades, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Token b2f57cf638b25ddd02390317de5017852342ab97'
+    }
+  });
+  const data = await res.json();
+
+  data.forEach(propiedad => {
+    array.push(propiedad);
+
+  });
+
+  return array;
+}  
